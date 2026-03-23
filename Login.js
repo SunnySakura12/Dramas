b4a87cee1ed7ba369e8e3e2396c46cfd500b6e7b
SunnySakura12 <nighstflower17@gmail.com>
2026-03-23 16:45:55 +0100

    Add Login component with local storage functionality

---
+++ Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Simulating authentication
+    if (username && password) {
+      localStorage.setItem('user', username);
+      alert('Login successful!');
+    } else {
+      alert('Please enter credentials.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
